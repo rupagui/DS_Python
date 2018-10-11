3281984e906c68e53bcf83d9c9c2d8287da89e99
Sergio Serrano Revuelto <serranos@MacBook-Pro-de-Sergio.local>
2018-10-11 21:26:09 +0200

    Añadida función para tratar Valores perdidos

--- srsergio/preprocesamiento.py
+++ srsergio/preprocesamiento.py
@@ -26,6 +26,50 @@ def procesaURL(url, method="GET", path="./", sep=",", delim="\n"):
         df = pd.DataFrame.from_records(datos1,columns=cabecera)
         return df
 
+def tratarNAN(df,nombreVar="",valorReemp=""):
+    '''Función para tratar los valores perdidos dentro del dataset.
+    Devuelve un dataset con los valores perdidos ya procesados
+    
+    Variables:
+         @paramdf -> Dataframe a procesar para eliminar valores perdidos
+         @param nombreVar -> Variables que queremos chequear para procesar valores perdidos
+    '''
+    df.dropna(axis=1,how="all")
+    df.dropna(axis=0,how="all").shape
+    if df[nombreVar].dtype != "int64" and df[nombreVar].dtype != "float64":
+        print("La variable es detectada como un objeto y no puede ser procesada")
+    else:
+        salida=False
+        while not salida:
+            procesado = int(input("""Selecciona el tipo de procesado: 
+                              \n\t1 - Substituir los NaN por el valor proporcionado {}
+                              \n\t2 - Substituir los NaN por la media
+                              \n\t3 - Substituir los NaN por la mediana
+                              \n\t4 - Substituir los NaN por el primer valor conocido hacia adelante
+                              \n\t5 - Substituir los NaN por el primer valor conocido hacia atrás
+                              """.format(valorReemp)))
+            if procesado == 1:
+                df = df[nombreVar].fillna(valorReemp)
+                salida=True
+            elif procesado == 2:
+                df[nombreVar]=df[nombreVar].fillna(df[nombreVar].mean())
+                salida=True
+            elif procesado == 3:
+                df[nombreVar]=df[nombreVar].fillna(df[nombreVar].median())
+                salida=True
+            elif procesado == 4:
+                df[nombreVar] = df[nombreVar].fillna(method="ffill")
+                salida=True
+            elif procesado == 5:
+                df[nombreVar] = df[nombreVar].fillna(method="bfill")
+                salida=True
+            else:
+                print("debe seleccionar una opción")
+    return df
+    
+    
+    
+    
 def crearDummies(df,nombreVar):
     dummy_var = pd.get_dummies(df[nombreVar], prefix=nombreVar)
     df = df.drop(columns=nombreVar)
